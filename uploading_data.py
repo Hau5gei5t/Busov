9f7bf6ad742960abddc1abd1b73221b64d9af62b
Hau5gei5t <busov.artem@yandex.ru>
2022-12-08 19:14:31 +0500

    Update uploading_data.py

--- uploading_data.py
+++ uploading_data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-def upload_chunks():
+
+def upload_chunks(file_name):
+    """
+    Принимает на вход общую выгрузку, а возвращает столько csv файлов сколько разных годов содержалось в выгрузке
+    """
     pd.set_option("expand_frame_repr", False)
-    df = pd.read_csv("vacancies_by_year.csv")
+    df = pd.read_csv(file_name)
     df["years"] = df["published_at"].apply(lambda s: int(s[:4]))
     years = df["years"].unique()
     for year in years:
@@ -10,5 +14,3 @@ def upload_chunks():
         data.iloc[:, :6].to_csv(rf"csv_files\year_{year}.csv", index=False)
     print(df.head(10))
     print(years)
-
-upload_chunks()
